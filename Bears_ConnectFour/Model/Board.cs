90d8ee8df7c648c5cc0fe66d191cc3903ca656db
hanse174 <hanse174@JB202-L22-W-D>
2018-09-11 14:45:51 -0400

    Working on CheckWin

--- Bears_ConnectFour/Model/Board.cs
+++ Bears_ConnectFour/Model/Board.cs
@@ -45,13 +45,126 @@ namespace Bears_ConnectFour
 
         /// <summary>
         /// determine if a player won the game
+        /// Function will return an Id of the winning player, or 0 if the move was not a winning move
         /// </summary>
         /// <returns>winning Pieces id</returns>
-        public int CheckWin()
-        {
-            //add code to determine winner
+        public int CheckWin( Piece grid, int [,] moveCoords ) {
 
-            return 1;
+            bool win = false;
+
+            // Get row
+            int r = moveCoords[0];
+
+            // Get Column
+            int c = moveCoords[1];
+
+            // Get the ID of the piece
+            int playerId = grid[r,c];
+
+            //
+            // Check Vertical
+            //
+            int piecesConnected = 0;
+            for (int r = 0; r < grid.GetLength(0); r++)
+			{
+                if (grid[r,c].Id == playerId)
+	            {
+                    piecesConnected++;
+                    if (piecesConnected == 4)
+	                {
+                        win = true;
+                        return playerId;
+	                }
+	            } else {
+                    piecesConnected = 0;
+                    }
+			}
+
+            //
+            // Check Horizontal
+            //
+            piecesConnected = 0;
+            for (int c = 0; c < grid.GetLength(1); c++)
+			{
+                if (grid[r,c].Id == playerId)
+	            {
+                    piecesConnected++;
+                    if (piecesConnected == 4)
+	                {
+                        win = true;
+                        return playerId;
+	                }
+	            } else {
+                    piecesConnected = 0;
+                    }
+			}
+
+            //
+            // Check NW to SE
+            //
+            piecesConnected = 0;
+            for (int i = r; i > 0; i--)
+			{
+                if ((i > grid.getLength(0)) && ())
+	            {
+
+	            }
+			}
+            // if row not out of bounds AND column not out of bounds, then decrement row and col val by 1
+
+
+
+
+
+
+
+
+
+
+            // 
+            if ((r-1) >= 0)
+	        {
+
+	        } else {
+
+            }
+
+            // Check currRow - 1
+
+            // Check currCol - 1
+
+
+
+
+
+
+            for (int i = 0; i < length; i++)
+			{
+                if (grid[r,(c + i)].Id == playerId)
+	            {
+                    piecesConnected++;
+	            }
+			}
+
+            // if(grid[r,(c + 1)].Id == playerId) => piecesConnected++;
+            // 
+            //
+            //
+            //
+            //
+            //
+            //
+
+
+            // Check NW to SE
+
+
+            if (win == true)
+            {
+                return playerId;
+            } else {
+                return -1;
+            }               
         }
     }
 }
